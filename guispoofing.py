3e90671359becc9d2432df92c59abf186ff24857
Chantalvdl <chantalvdluijtgaarden@live.nl>
2019-02-27 23:00:34 +0100

    making menus

--- guispoofing.py
+++ guispoofing.py
@@ -1,17 +1,28 @@
 from tkinter import *
 
+
 screen = Tk()
-text = Label(screen, text="Spoofing is leuk!")
-text.pack(side=BOTTOM)
-bottomframe = Frame(screen)
-bottomframe.pack(side=TOP)
-buttonSelect = Button(bottomframe, text="Select", fg="blue")
-buttonSelect.pack(side=TOP)
-one = Label(screen, text="hoi", bg="red", fg="white")
-one.pack()
-two = Label(screen, text="hoi2", bg="red", fg="white")
-two.pack(fill=X)
-three = Label(screen, text="hoi3", bg="red", fg="white")
-three.pack(side=LEFT, fill=Y)
+
+# dropdown menu
+# menu = Menu(screen)
+# screen.config(menu=menu)
+# submenu = Menu(menu)
+# menu.add_cascade(label="Spoofing", menu=submenu)
+# submenu.add_command(label="spoof!")
+
+
+# text = Label(screen, text="Spoofing is leuk!")
+# text.pack(side=BOTTOM)
+# bottomframe = Frame(screen)
+# bottomframe.pack(side=TOP)
+# buttonSelect = Button(bottomframe, text="Select", fg="blue", command=scanning.scanning)
+# buttonSelect.bind("<Button-1>", def name)
+# buttonSelect.pack(side=TOP)
+# one = Label(screen, text="hoi", bg="red", fg="white")
+# one.pack()
+# two = Label(screen, text="hoi2", bg="red", fg="white")
+# two.pack(fill=X)
+# three = Label(screen, text="hoi3", bg="red", fg="white")
+# three.pack(side=LEFT, fill=Y)
 
 screen.mainloop()
