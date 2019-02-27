8452e218e70da138ea75dd04e8c08af9e0442ba0
Chantalvdl <chantalvdluijtgaarden@live.nl>
2019-02-27 21:53:52 +0100

    making BUTTONS

--- guispoofing.py
+++ guispoofing.py
@@ -1,6 +1,10 @@
 from tkinter import *
 
 screen = Tk()
-text = Label(screen, text = "Spoofing is leuk!")
-text.pack()
+text = Label(screen, text="Spoofing is leuk!")
+text.pack(side=BOTTOM)
+bottomframe = Frame(screen)
+bottomframe.pack(side=TOP)
+buttonSelect = Button(bottomframe, text="Select", fg="blue")
+buttonSelect.pack(side=TOP)
 screen.mainloop()
