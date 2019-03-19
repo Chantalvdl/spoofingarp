81ef9f949ff8b6a440681f30419c1c7a0aaf0bc7
Chantalvdl <chantalvdluijtgaarden@live.nl>
2019-03-19 16:26:21 +0100

    aaaaah2

--- scanning.py
+++ scanning.py
@@ -1,43 +1,20 @@
 from scapy.all import *
 
 
-
 def network_scanning():
     networklist = []
-    for network, netmask, ignore, interface, address in scapy.config.conf.route.routes:
+    for networkname, netmask, ignore, interfacename, ipaddress in scapy.config.conf.route.routes:
 
         # Skip standard interfaces and invalid netmasks.
-        if network == 0 or interface == 'lo' or address == '127.0.0.1' or address == '0.0.0.0' or netmask <= 0 or netmask == 0xFFFFFFFF:
-            continue
+        # if network == 0 or interface == 'lo' or address == '127.0.0.1' or address == '0.0.0.0' or netmask <= 0 or netmask == 0xFFFFFFFF:
+        #     continue
 
         # Format the net/ip
-        net = format_ip(network, netmask)
+        net = format_ip(networkname, netmask)
         if net:
-            networklist += [net + ", " + interface]
+            networklist += [net + ", " + interfacename]
 
     return networklist
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
